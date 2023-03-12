a3cdf3fd511cd75597b57e7477d1a7d4ae326e2a
Linus <64785287+joao-lino@users.noreply.github.com>
2023-03-12 20:08:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -212,3 +212,38 @@ function addTodoItem() {
 
 
 */
+
+// Add a "checked" symbol when clicking on a list item
+var list = document.getElementById('myUL');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+// Create a new list item when clicking on the "Add" button
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("todo-input").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("You must write something!");
+  } else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("todo-input").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
